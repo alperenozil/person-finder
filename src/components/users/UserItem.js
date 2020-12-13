9ace2289cf2d87a37d5f0a036429aeffedbbe94f
Necdet Alperen Özil <48528832+alperenozil@users.noreply.github.com>
2020-12-13 15:09:15 +0300

    Update UserItem.js

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-    constructor(){
-        super();
-        this.state={
-            id:"id",
-            login:"loginname",
-            avatar_url:"https://pbs.twimg.com/profile_images/1217835969988169728/sClA2Lef_400x400.jpg",
-            html_url:"other url"
-        }
+    state={
+        id:"id",
+        login:"loginname",
+        avatar_url:"https://pbs.twimg.com/profile_images/1217835969988169728/sClA2Lef_400x400.jpg",
+        html_url:"other url"
     }
     render() {
+        const {id, login, avatar_url, html_url}=this.state;
         return (
             <div className="card text-center">
                 User Item
                 <p></p>
-                <img src={this.state.avatar_url} className="round-img" style={{width:"120px"}}></img>
+                <img src={avatar_url}
+                className="round-img"
+                style={{width:"120px"}}></img>
+                <h3>{login}</h3>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">Details</a>
+            </div>
             </div>
         )
     }
